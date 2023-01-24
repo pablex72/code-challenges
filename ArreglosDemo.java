b08145ac02c6562f20a466a82c3953ef83908bf2
pablex72 <tonyp2hoy@gmail.com>
2023-01-24 18:53:03 -0400

    demo array insert element avoid leave empty

--- ArreglosDemo.java
+++ ArreglosDemo.java
@@ -464,7 +464,47 @@ class ArrayProb11{
             System.out.println(arreglo[i] + "-");
         }
     }
+}
+class ArrayProb12{
+    /**
+     * Read by keyboard a table of 10 numerical elements and position between 0 and 9
+     * delete the element in the postion given without leave holes* *
+     */
+    public static void ex12(){
+        Scanner entrada = new Scanner(System.in);
 
+        int[] tabla = new int[10];
+        int posicion;
+
+        //Ingresar los números por medio de la consola.
+        System.out.println("INGRESE NÚMEROS DE LA TABLA");
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Número " +i+ ": ");
+            tabla[i] = entrada.nextInt();
+        }
+
+        //Solicitar una posición y comprobar que está entre 0-9.
+        do {
+            System.out.print("\nDigite una posición entre 0-9: ");
+            posicion = entrada.nextInt();
+        } while (posicion < 0 || posicion > 10);
+
+        //Colocar cero la posición que se digito.
+        tabla[posicion] = 0;
+
+        //Recorrer todos los números de la tabla para no dejar huecos.
+        for (int i = posicion; i < 9; i++) {
+            tabla[i] = tabla[i+1];
+        }
+
+        //Poner en cero la último posición.
+        tabla[9] = 0;
+
+        System.out.println("\nNUEVA TABLA");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Número " +i+ ": " +tabla[i]);
+        }
+    }
 }
 
 public class ArreglosDemo {
@@ -478,6 +518,7 @@ public class ArreglosDemo {
         //ArrayProb3b.ex3b();
         //ArrayProb7.ex7();
         //ArrayProb8.ex8();
-        ArrayProb11.ex11();
+        //ArrayProb11.ex11();
+        ArrayProb12.ex12();
 }}
 
