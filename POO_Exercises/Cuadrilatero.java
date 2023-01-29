391ef5f99d1abd0ac9bca54d33158c81b6594dfe
pablex72 <tonyp2hoy@gmail.com>
2023-01-29 02:09:59 -0400

    demo exercise POO cuadrilatero

---
+++ POO_Exercises/Cuadrilatero.java
@@ -0,0 +1,35 @@
+package POO_Exercises;
+
+/**
+ * Ejercicio 1: Construir un programa que calcule el área y el perímetro de un cuadrilátero
+ * * dada la longitud de sus dos lados. Los valores de la longitud deberán introducirse por línea de ordenes.
+ * * Si es un cuadrado, sólo se proporcionará la longitud de uno de los lados al constructor.*
+ */
+public class Cuadrilatero {
+    //Atributos
+    private float lado1;
+    private float lado2;
+
+    //Metodos
+
+    //Metodo constructor 1 (cuadrilatero)
+
+    public Cuadrilatero(float lado1, float lado2) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }
+    // funciona cuando es cuadrado
+    public Cuadrilatero(float lado1) {
+        this.lado1 = this.lado1 = lado1;
+    }
+
+    public float getPerimetro(){
+        float perimetro = 2 * (lado1 + lado2);
+        return perimetro;
+    }
+
+    public float getArea(){
+        float area = (lado1 * lado2);
+        return  area;
+    }
+}
---
+++ POO_Exercises/Principal.java
@@ -0,0 +1,21 @@
+package POO_Exercises;
+
+import javax.swing.*;
+
+public class Principal {
+    public static void main(String[] args) {
+        Cuadrilatero c1;
+        float lado1,lado2;
+        lado1 = Float.parseFloat(JOptionPane.showInputDialog("digite 1 "));
+        lado2 = Float.parseFloat(JOptionPane.showInputDialog("digite 2 "));
+
+        if(lado1 == lado2){
+            c1 = new Cuadrilatero(lado1);
+        }else {
+            c1 = new Cuadrilatero(lado1, lado2);
+        }
+
+        System.out.println("El perimetro es "+ c1.getPerimetro());
+        System.out.println("El area es "+ c1.getArea());
+    }
+}
