03f85c4c4f41699324fe00458b112d4b6a7ac6c4
pablex72 <tonyp2hoy@gmail.com>
2023-02-11 23:46:54 -0400

    challenge hack ranker file

---
+++ hackerRank/ArrayManipulation.java
@@ -0,0 +1,14 @@
+package hackerRank;
+
+import java.util.List;
+
+public class ArrayManipulation {
+    public static void main(String[] args) {
+
+    }
+
+    public static long arrayManipulation(int n, List<List<Integer>> queries) {
+        // Write your code here
+
+    }
+}
