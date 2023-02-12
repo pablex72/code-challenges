8c4d8b508eb1e07b43f9dcc4b9e6023cf8f6f515
pablex72 <tonyp2hoy@gmail.com>
2023-02-11 23:51:12 -0400

    challenge hackrank array manipulation List

--- hackerRank/ArrayManipulation.java
+++ hackerRank/ArrayManipulation.java
@@ -1,5 +1,6 @@
 package hackerRank;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayManipulation {
@@ -9,6 +10,18 @@ public class ArrayManipulation {
 
     public static long arrayManipulation(int n, List<List<Integer>> queries) {
         // Write your code here
+        long max = 110;
+        int rowSize = 5;
+        int colSize = 3;
+        List<List<Integer>> arr = new ArrayList<List<Integer>>();
+        for (int i = 0; i < rowSize; i++) {
+            List<Integer> arrRow = new ArrayList<Integer>();
+            for (int j = 0; j < colSize; j++) {
+              //  arrRow.add(x);
+            }
+            arr.add(arrRow);
+        }
 
+        return max;
     }
 }
